0bc86034b440ab54c3352055f6278a4d7433460d
Alison Gullifer <aligullifer@gmail.com>
2026-06-20 05:48:15 +0000

    updates to final

--- chapter08/project1/js/ch08-proj01.js
+++ chapter08/project1/js/ch08-proj01.js
@@ -2,5 +2,35 @@
 const tax_rate = prompt('Enter tax rate (0.10)');
 const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
-/* add loop and other code here ... in this simple exercise we are not
-   going to concern ourselves with minimizing globals, etc */
+let subtotal = 0;
+
+for (let i = 0; i < cart.length; i++) {
+   const item = cart[i];
+   const total = calculateTotal(item.quantity, item.product.price);
+   subtotal += total;
+   outputCartRow(item, total);
+}
+
+const tax = subtotal * tax_rate;
+const shipping = subtotal > shipping_threshold ? 0 : 40;
+const grandTotal = subtotal + tax + shipping;
+
+document.write('<tr class="totals">');
+document.write('<td colspan="4">Subtotal</td>');
+document.write('<td>$' + subtotal.toFixed(2) + '</td>');
+document.write('</tr>');
+
+document.write('<tr class="totals">');
+document.write('<td colspan="4">Tax</td>');
+document.write('<td>$' + tax.toFixed(2) + '</td>');
+document.write('</tr>');
+
+document.write('<tr class="totals">');
+document.write('<td colspan="4">Shipping</td>');
+document.write('<td>$' + shipping.toFixed(2) + '</td>');
+document.write('</tr>');
+
+document.write('<tr class="totals">');
+document.write('<td colspan="4" class="focus">Grand Total</td>');
+document.write('<td class="focus">$' + grandTotal.toFixed(2) + '</td>');
+document.write('</tr>');
